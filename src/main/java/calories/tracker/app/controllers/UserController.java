bcb1e3418fd3c8d1eabbfebbb44c101970d5dc18
Harpreet28 <hkamboj02@gmail.com>
2016-12-18 00:51:44 -0600

    Updated comments section

--- src/main/java/calories/tracker/app/controllers/UserController.java
+++ src/main/java/calories/tracker/app/controllers/UserController.java
@@ -17,14 +17,8 @@ import java.security.Principal;
 /**
  *
  *  REST service for users.
- * New comment 
- * another commmet 
- * comment from Harpreet
- * comments from sunny
- * comment from sukhchain
- * again
- * updated on 12/17/2016
- * from sukhchain
+ *  All the unnecassry comments have been deleted by Harpreet
+ * 
  */
 
 @Controller
